9d4d996f6b506aeef3beb603a6e0191b021aa9f1
Mario Lubenka <me@saitho.me>
2023-11-28 19:10:39 +0100

    fix: export Command

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,6 +1,7 @@
 export {Cli} from './Cli';
 export {Options} from './TOptions';
 export {AbstractCommand} from './AbstractCommand';
+export {Command} from './Command';
 export {SuccessResponse} from './SuccessResponse';
 export {ErrorResponse} from './ErrorResponse';
 export {name, command, description, argument} from './Decorators';
